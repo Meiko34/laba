b862d4dc74a1ae862e0de15bcaed9533b67b1749
Meiko <kurteupovas@gmail.com>
2025-01-09 14:51:49 +0300

    Добавление обработки  цветов с помощью ANSI-кодов

--- laba12.cpp
+++ laba12.cpp
@@ -8,6 +8,20 @@ using namespace std;
 const int COLORS = 7; // Количество цветов
 const char COLOR_CHARS[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G'}; // Символы для цветов
 
+// ANSI-коды для цветов
+const string COLOR_CODES[] = {
+"\033[31m", // Красный
+"\033[32m", // Зеленый
+"\033[33m", // Желтый
+"\033[34m", // Синий
+"\033[35m", // Магента
+"\033[36m", // Циан
+"\033[37m"  // Белый
+};
+const string RESET_COLOR ="\033[0m";
+
+
+
 int main() {
     srand(static_cast<unsigned>(time(0))); // Инициализация генератора случайных чисел
 
@@ -24,10 +38,10 @@ int main() {
             } }
 
     // Вывод матрицы с цветами
-    cout <<"Сгенерированная матрица:\n";  
+     cout <<"Сгенерированная матрица:\n";  
     for (const auto& row : matrix) {
         for (char color : row) {
-            cout << color <<""; // Выводим букву цвета
+            int colorIndex = color - 'A'; // Получаем индекс цвета
+            cout << COLOR_CODES[colorIndex] << color <<"" << RESET_COLOR; // Выводим букву с ее цветом
             } cout << endl;} 
-            return 0;
-            }
+            return 0;}
